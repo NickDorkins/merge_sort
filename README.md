a92f2d345bca376e02b55ad0dedd3f635dde13e5
NickDorkins <nick.dorkins@gmail.com>
2021-03-07 13:56:45 -0800

    Complete

---
+++ README.md
@@ -0,0 +1,1 @@
+[Pull Request](https://github.com/NickDorkins/merge_sort/pull/1)
--- tests/test_merge_sort.py
+++ tests/test_merge_sort.py
@@ -1,5 +1,19 @@
-from merge_sort import __version__
+
+from merge_sort.merge_sort import merge_sort
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+
+def test_unsorted_list():
+    expected = [4, 8, 15, 16, 23, 42]
+    actual = merge_sort([8, 4, 23, 42, 16, 15])
+    assert actual == expected
+
+def test_unsorted_list_with_negatives():
+    expected = [-16, -4, 8, 15, 23, 42]
+    actual = merge_sort([8, -4, 23, 42, -16, 15])
+    assert actual == expected
+
+def test_short_list():
+    expected = "2 or more numbers needed to sort"
+    actual = merge_sort([8])
+    assert actual == expected
